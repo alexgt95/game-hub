564efeaf1b547c7a650c3c632b29f55342efa1dd
alexgt95 <40178272+alexgt95@users.noreply.github.com>
2026-08-26 20:38:18 -0600

    Show a spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,6 @@
-import { HStack, Icon, List, Skeleton, Text } from '@chakra-ui/react';
+import { HStack, Icon, List, Skeleton, Spinner, Text } from '@chakra-ui/react';
 import { BsController } from 'react-icons/bs';
 import useGenres from '../hooks/useGenres';
-
-const SKELETON_COUNT = 10;
 
 const GenreList = () => {
   const { data: genres, error, isLoading } = useGenres();
@@ -12,23 +10,15 @@ const GenreList = () => {
       {error && <Text color="red.400">{error}</Text>}
       <List.Root variant="plain" gap={3}>
         {isLoading &&
-          Array.from({ length: SKELETON_COUNT }, (_, index) => (
-            <List.Item key={index} paddingY="5px">
-              <HStack>
-                <Skeleton boxSize="32px" borderRadius="md" />
-                <Skeleton height="16px" width="120px" />
-              </HStack>
-            </List.Item>
-          ))}
-        {!isLoading &&
-          genres.map((genre) => (
-            <List.Item key={genre.id} paddingY="5px">
-              <HStack>
-                <Icon as={BsController} boxSize="8" color="gray.500" />
-                <Text fontSize="lg">{genre.name}</Text>
-              </HStack>
-            </List.Item>
-          ))}
+            <Spinner />}
+        {!isLoading && genres.map((genre) => (
+          <List.Item key={genre.id} paddingY="5px">
+            <HStack>
+              <Icon as={BsController} boxSize="8" color="gray.500" />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </List.Item>
+        ))}
       </List.Root>
     </>
   );
